ec816758bfcc5ca8ee98c37a132b708c16487dab
nicoksa <nicolascasarino@gmail.com>
2024-07-30 20:05:16 -0300

    Add files via upload

---
+++ NegocioDetalleVenta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+    public class NegocioDetalleVenta
+    {
+        DaoDetalleVenta dao = new DaoDetalleVenta();
+        public DataTable getTabla(Venta venta)
+        {
+            return dao.getTablaDetalleVenta(venta);
+        }
+
+       public bool CargarDetalleVenta(DetalleVenta detalleVenta)
+        {
+
+            int filasAfectadas = 0;
+
+            filasAfectadas = dao.CargarDetalleVenta(detalleVenta);
+
+            if (filasAfectadas == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+
+    }
+}
